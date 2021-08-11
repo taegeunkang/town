cbacf28eb4eea6343e9975c59f254078f67bbff9
Dean Kang <pregea@icloud.com>
2021-08-11 19:43:31 +0900

    210811

--- src/main/java/com/example/springsocial/domain/Image.java
+++ src/main/java/com/example/springsocial/domain/Image.java
@@ -2,12 +2,14 @@ package com.example.springsocial.domain;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
 
 @Getter
 @Setter
+@NoArgsConstructor
 @Entity
 public class Image {
     @Id
